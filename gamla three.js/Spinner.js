a71ca6d24169018e54dc1e768930de2ca977c9db
emmaweberyd <emma.weberyd@gmail.com>
2018-02-09 15:29:55 +0100

    Implement model

---
+++ gamla three.js/Spinner.js
@@ -0,0 +1,19 @@
+class Spinner {
+
+	constructor(diameter,inertia,friction){
+		this.diameter = diameter; 
+		this.friction = friction; 
+		this.inertia = inertia;
+		this.angularPosition = 0;
+		this.angularVelocity = 0;
+		this.counter = 0; // counts how many iterations
+	}
+
+	spin(force, stepLength) { // Updates angular position with euler
+		var angularAcceleration = (1/(this.inertia)) * (this.diameter*force - this.friction*this.angularVelocity);
+		this.angularVelocity = this.angularVelocity + stepLength*angularAcceleration;
+		this.angularPosition = this.angularPosition + stepLength*this.angularVelocity;
+
+	}
+
+}
